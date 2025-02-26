c9147b1ec714b2cfb552fecc6816d42c8cc6508a
Ryan Estácio <119369302+ryannestacio@users.noreply.github.com>
2025-02-26 00:21:07 -0300

    Primeiro commit

---
+++ android/app/src/main/kotlin/com/example/desafio_column_and_row/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.desafio_column_and_row
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(DesefioRowColumn());
+}
+
+class DesefioRowColumn extends StatelessWidget {
+  const DesefioRowColumn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.teal[900],
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/images/eu.jpg'),
+                radius: 60,
+              ),
+            ),
+            Text(
+              'Ryan Estácio',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              '_______________________________________________',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              '82 9 8219-9052',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                overflow: TextOverflow.clip,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
